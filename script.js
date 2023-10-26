6b803dc4ce8e32c10d03637bc6646505ab617109
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-10-26 09:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
-
-function compare(a, b) {
-  if (a < b) {
-      return -1;
-  } else if (a > b) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
 function calculateMinCost() {
-  //your code here
+  let str = document.getElementById('rope-lengths').value;
   
-  //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
-  
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
+  if (!str) {
+    alert("Please enter rope lengths.");
+    return;
   }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
+
+  let arr = str.split(",").map(str => parseInt(str));
+
+  if (arr.length < 2) {
+    alert("At least two rope lengths are required.");
+    return;
+  }
+
+  let total = 0;
+  arr = arr.sort((a, b) => a - b);
+
+  while (arr.length >= 2) {
+    let sum = arr[0] + arr[1];
+    let rem = [sum, ...arr.slice(2)];
+    rem = rem.sort((a, b) => a - b);
+    arr = rem;
+    total += sum;
+  }
+
+  let result = document.getElementById('result');
+  result.innerHTML = total;
 }
-//console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
-}  
-  
-  
